c5e27b951dc1019604c18ad66e90ac3e2f7866a5
DivMaestro <m.mustafoff00@gmail.com>
2024-01-28 20:16:27 +0200

    Initial commit

---
+++ js/thumbnails-fetcher.js
@@ -0,0 +1,46 @@
+// Fetch JSON data
+fetch("JSON/thumbnails.json")
+  .then((response) => response.json())
+  .then((data) => {
+    // Call a function to create thumbnails using the fetched data
+    createThumbnails(data.thumbnails);
+  })
+  .catch((error) => console.error("Error fetching JSON:", error));
+
+// Function to create thumbnail elements
+function createThumbnails(thumbnails) {
+  let thumbnailContainer = document.getElementById("thumbnail-container");
+
+  // Loop through the thumbnails and create elements
+  thumbnails.forEach((thumbnail) => {
+    let thumbnailElement = document.createElement("div");
+    thumbnailElement.className = "col-lg-6 col-12";
+    thumbnailElement.innerHTML = `
+    <div class='thumbnail'>
+    <div class='thumbnail-image'>
+    <img src="${thumbnail.src}" alt="${thumbnail.heading}">
+    </div>
+    <div class='caption'>
+    <span>${thumbnail.category}</span>
+    <h3>${thumbnail.heading}</h3>
+    <p>${thumbnail.subject}</p>
+    </div>
+    <div class="time-and-views">
+    <div class="published-day">
+      <div class="icon-container">
+        <img src="icons/calendar.svg" alt="SVG Icon" />
+      </div>
+      <span>${thumbnail.published}</span>
+    </div>
+    <div class="views">
+      <div class="icon-container">
+        <img src="icons/eye.svg" alt="SVG Icon" />
+      </div>
+      <span>${thumbnail.views}</span>
+    </div>
+    </div>
+  </div>
+  `;
+    thumbnailContainer.appendChild(thumbnailElement);
+  });
+}
---
+++ pages/home.php
@@ -0,0 +1,8 @@
+<div class="container-fluid">
+  <div class="row banner-container">
+    <div class="col-sm-7 yellow"></div>
+    <div class="col-sm-5" id="thumbnail-container">
+      <h3>Вижте също</h3>
+    </div>
+  </div>
+</div>
